83bab88e540e5144750118dcf4d919d4b4e1665a
Conor McCarthy <concoolmccarthy@gmail.com>
2013-01-26 14:27:38 +0000

    Yo
    Yo

---
+++ Move.java
@@ -0,0 +1,58 @@
+package Gamma;
+import robocode.*;
+
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Move - a robot by (your name here)
+ */
+public class Move extends Robot
+{
+	/**
+	 * run: Move's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			turnGunRight(360);
+			
+			
+	    }
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+		double bearing;
+	    bearing=e.getBearing();
+}
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}	
+}
+																
---
+++ Propermove.java
@@ -0,0 +1,53 @@
+package Gamma;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Propermove - a robot by (your name here)
+ */
+public class Propermove extends Robot
+{
+	/**
+	 * run: Propermove's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+	        ahead(100);
+     }
+}
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		
+	}	
+}
+								
